0991aad139b385a4182bff0d718ed9db31f1e191
SandroScheuermann <sandroryze@gmail.com>
2021-05-10 14:51:38 -0300

    Add project files.

---
+++ Triangulo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite o vértice X do triângulo");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nDigite o vértice Y do triângulo");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nDigite o vértice Z do triângulo");
+                int z = Convert.ToInt32(Console.ReadLine());
+
+                Triangulo triangulo = new Triangulo(x, y, z);
+
+                if (triangulo.ValidaTriangulo())
+                {
+                    Console.Clear();
+                    Console.WriteLine("O triângulo é : " + triangulo.VerificaTipo());                    
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Triângulo inválido!");                    
+                }
+                continue;
+            }
+        }
+    }
+}
---
+++ Triangulo/Triangulo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Triangulo
+    {
+        private int x, y, z;
+        public Triangulo(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Z { get => z; set => z = value; }
+
+        public bool ValidaTriangulo()
+        {
+            bool validacao = false;
+
+            if (X == 0 || Y == 0 || Z == 0)
+                validacao = false;
+            else
+                validacao = true;
+
+            return validacao;
+        }
+
+        public string VerificaTipo()
+        {
+            string tipoTriangulo = null;
+
+            if (X == Y && X == Z && Y == Z)
+                tipoTriangulo = "Equilátero\n";
+            else if ((X == Y & X != Z) || (X == Z && X != Y) || (Y == Z & X != Z))
+                tipoTriangulo = "Isosceles\n";
+            else
+                tipoTriangulo = "Escaleno\n";
+
+            return tipoTriangulo;
+        }
+
+    }
+}
